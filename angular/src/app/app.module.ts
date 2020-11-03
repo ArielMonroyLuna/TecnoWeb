ab6abde4aaaa1452c3d35bbb7c8c037700aef913
ArielMonroyLuna <ariel.monroy@ucb.edu.bo>
2020-11-02 21:01:37 -0400

    Trabajo terminado

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContComponent } from './cont/cont.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContComponent,
+
   ],
   imports: [
     BrowserModule,
